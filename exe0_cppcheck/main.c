808cf248e56392e6552c3cc5468301b956f46e51
David Chung <davidyhc@al.insper.edu.br>
2026-02-20 11:47:04 -0300

    Teste

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -8,11 +8,12 @@ int multiplyNumbers(int x, int y) {
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 5;
+    int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
     }
 
